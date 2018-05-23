b24b74fe26252c8e811f20d4dbae8f0f7c92d31a
Luciano <lucianopereira2009@gmail.com>
2018-05-22 23:31:56 -0400

    to check that Colina

---
+++ app/src/main/java/com/example/luciano/myapplication/Main2Activity.java
@@ -0,0 +1,176 @@
+package com.example.luciano.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+    int contador2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView (R.layout.activity_main2);
+        Button btn1 = (Button) findViewById(R.id.button2);
+        btn1.setOnClickListener(this); // Habilito los botones
+        Button btn2 = (Button) findViewById(R.id.button3);
+        btn2.setOnClickListener(this); // Habilito los botones
+        Button btn3 = (Button) findViewById(R.id.button4);
+        btn3.setOnClickListener(this); // Habilito los botones
+        Button btn4 = (Button) findViewById(R.id.button5);
+        btn4.setOnClickListener(this); // Habilito los botones
+        Button btn5 = (Button) findViewById(R.id.button6);
+        btn5.setOnClickListener(this); // Habilito los botones
+        Button btn6 = (Button) findViewById(R.id.button9);
+        btn6.setOnClickListener(this); // Habilito los botones
+        Button btn7 = (Button) findViewById(R.id.button10);
+        btn7.setOnClickListener(this); // Habilito los botones
+        Button btn8 = (Button) findViewById(R.id.button12);
+        btn8.setOnClickListener(this); // Habilito los botones
+        Button btn9 = (Button) findViewById(R.id.button13);
+        btn9.setOnClickListener(this); // Habilito los botones
+        Button btn10 = (Button) findViewById(R.id.button14);
+        btn10.setOnClickListener(this); // Habilito los botones
+        Button btn11 = (Button) findViewById(R.id.button15);
+        btn11.setOnClickListener(this); // Habilito los botones
+        Button btn12 = (Button) findViewById(R.id.button16);
+        btn12.setOnClickListener(this); // Habilito los botones
+        Button btn13 = (Button) findViewById(R.id.button17);
+        btn13.setOnClickListener(this); // Habilito los botones
+        Button btn14 = (Button) findViewById(R.id.button18);
+        btn14.setOnClickListener(this); // Habilito los botones
+        Button btn15 = (Button) findViewById(R.id.button19);
+        btn15.setOnClickListener(this); // Habilito los botones
+        Button btn16 = (Button) findViewById(R.id.button20);
+        btn16.setOnClickListener(this); // Habilito los botones
+        Button btn17 = (Button) findViewById(R.id.button21);
+        btn17.setOnClickListener(this); // Habilito los botones
+        Button btn18 = (Button) findViewById(R.id.button22);
+        btn18.setOnClickListener(this); // Habilito los botones
+        Button btn19 = (Button) findViewById(R.id.button23);
+        btn19.setOnClickListener(this); // Habilito los botones
+        Button btn20 = (Button) findViewById(R.id.button24);
+        btn20.setOnClickListener(this); // Habilito los botones
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent2 = new Intent(this, MainActivity.class);
+        Bundle mibundle = new Bundle();
+        contador2 = contador2 + 1;
+        mibundle.putInt("count",contador2);
+        intent2.putExtra("oh",mibundle);
+
+        switch (v.getId()) {
+
+            case R.id.button2:
+                String producto = "arroz";
+
+                intent2.putExtra("dato", producto); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button3:
+                String producto1 = "mantequilla";
+
+                intent2.putExtra("dato", producto1); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button4:
+                String producto2 = "pan";
+
+                intent2.putExtra("dato", producto2); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button5:
+                String producto3 = "papas";
+
+                intent2.putExtra("dato", producto3); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button6:
+                String producto4 = "queso";
+                intent2.putExtra("dato", producto4); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button9:
+                String producto5 = "maiz";
+                intent2.putExtra("dato", producto5); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button10:
+                String producto6 = "huevos";
+                intent2.putExtra("dato", producto6); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button21:
+                String producto7 = "flips";
+                intent2.putExtra("dato", producto7); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button12:
+                String producto8 = "leche";
+                intent2.putExtra("dato", producto8); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button13:
+                String producto9 = "salsa";
+                intent2.putExtra("count", contador2);
+                intent2.putExtra("dato", producto9); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button14:
+                String producto10 = "mayonesa";
+                intent2.putExtra("dato", producto10); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button15:
+                String producto11 = "harinatrigo";
+                intent2.putExtra("count", contador2);
+                intent2.putExtra("dato", producto11); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button16:
+                String producto12 = "cocacola";
+                intent2.putExtra("dato", producto12); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button17:
+                String producto17 = "cebolla";
+                intent2.putExtra("dato", producto17); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button18:
+                String producto18 = "pimenton";
+                intent2.putExtra("dato", producto18); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button19:
+                String producto19 = "salchicha";
+                intent2.putExtra("dato", producto19); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button20:
+                String producto20 = "tocineta";
+                intent2.putExtra("dato", producto20); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button22:
+                String producto21 = "jamon";
+                intent2.putExtra("dato", producto21); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button23:
+                String producto22 = "tequeños";
+                intent2.putExtra("dato", producto22); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+            case R.id.button24:
+                String producto23 = "cambur";
+                intent2.putExtra("dato", producto23); //envio los datos, del otro lado buscare dato no producto
+                startActivity(intent2);
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/luciano/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.luciano.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView textView;
+    Button boton;
+    int contador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton = (Button) findViewById(R.id.button7); //colocamos el id del boton
+        boton.setOnClickListener(this); //con esto habilito el boton
+
+        Intent intent2 = getIntent(); //recibimos el intent es para cachar lo que viene
+        Bundle bundle = intent2.getExtras(); //para obtener los extras del intent
+        Bundle mibundle = this.getIntent().getExtras();
+       if (bundle!=null) {
+          contador = mibundle.getInt("oh");
+       }
+        switch (contador) {
+            case 0:
+                textView = (TextView)findViewById(R.id.textView11); //enlazamos el text view con su id
+                break;
+            case 1:
+                textView = (TextView)findViewById(R.id.textView); //enlazamos el text view con su id
+                break;
+            default:
+                break;
+        }
+        if (bundle!=null){
+            String cadena = (String) bundle.get("dato"); //nos aseguramos que no venga nulo para no haber error
+
+            textView.setText(cadena); //lo transformo en cadena
+            Toast toast1 = Toast.makeText(getApplicationContext(), contador).show();
+            toast1.show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {//aqui se creo el medodo onclick porque estoy implementando on clicklistener
+        switch (v.getId()) { // aqui obtengo lo que viene del metodo on click
+            case R.id.button7:
+                Intent intent = new Intent(this,Main2Activity.class); //inten que llama a Main2Activity
+                startActivity(intent); //con esta sentencia incio el intent de arriba
+                break;
+            default:
+                break;
+
+        }
+    }
+}
